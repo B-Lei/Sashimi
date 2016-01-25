e61e0748cf545ae02af43a47d7ad93dd9922a1ae
Ben Lei <benjamin.g.lei@gmail.com>
2016-01-24 22:43:43 -0800

    Initial commit

---
+++ android/src/com/sashimi/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sashimi.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sashimi.game.Sashimi;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Sashimi(), config);
+	}
+}
---
+++ desktop/src/com/sashimi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sashimi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sashimi.game.Sashimi;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sashimi(), config);
+	}
+}

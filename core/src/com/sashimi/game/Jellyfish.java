c723d75fa2eaf41b7ecaeca95d36ea4a154e6b27
Ben Lei <benjamin.g.lei@gmail.com>
2016-03-07 09:51:36 -0800

    bl - Level1 now uses enemy classes that inherit from RandomEnemy

---
+++ core/src/com/sashimi/game/Jellyfish.java
@@ -0,0 +1,9 @@
+package com.sashimi.game;
+
+public class Jellyfish extends RandomEnemy {
+    public Jellyfish(GameScreen screen, int x, int y) {
+        super(screen,x,y,"jelly1.5x.png");
+        moveSpeed = 5;
+        bulletVelocity = -6;
+    }
+}

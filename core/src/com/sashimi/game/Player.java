e6c518b2b2ed78d4efdc0f65ccbd2291c51ba85c
Ben Lei <benjamin.g.lei@gmail.com>
2016-03-07 06:49:07 -0800

    bl - bugfix: fixed scoring system, no longer overflows

--- core/src/com/sashimi/game/Player.java
+++ core/src/com/sashimi/game/Player.java
@@ -57,12 +57,9 @@ public class Player extends Entity {
         return hitbox.overlaps(other);
     }
 
-    // Currently has a bug that causes score to be negative lower bound of integer
-    // FOUND: this occurs when you hit two enemies at the same time, so deltaTime is 0 - and when
-    // you divide by 0, you overflow the score. To solve this, you can just add a value to it as a baseline.
     public void setScore(double currentTime){
         double deltaTime = currentTime - prevHitTime;
-        score = score + (int)(1000.0/(1 + deltaTime));
+        score = score + (int)(1000.0/(1 + deltaTime)); // 1 is in case deltaTime is 0
         prevHitTime = currentTime;
         System.out.println("Time difference: " + deltaTime);
         System.out.println("Score: " + score);
